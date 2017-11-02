44acc887611a9f8bef5c4e29b790581febedbf7a
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-11-02 18:40:30 +0000

    added gulp, for compiling a pre processed css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import header from './header'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header />
       </div>
     );
   }
---
+++ src/header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Header extends React.Component{
+      render(){
+        return(
+        <Header>
+          <div className="Logo">
+         Logo
+         </div>
+
+         <nav>
+         `  <ul>
+            </ul>
+              <li>"
+               <a href ="#"> Home /a>
+              </li>
+              <li>"
+               <a href ="#"> Players /a>
+              </li>
+              <li>"
+               <a href ="#"> Teams /a>
+              </li>
+         </nav>
+        </Header>
+        );
+      }
+    }
+
+ export default Header;
