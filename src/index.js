a488bf9ae5ed50c788145b71aafa0413af42e037
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-11-07 03:07:18 +0000

    got rid of apps.js, which funcuality was inserted into index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+  } from 'react-router-dom';
+
+
+//components
+import Header from './components/headerComponent/header';
+import Homepage from './components/pages/homePage';
+import Team from './components/pages/team';
+import Facts from './components/pages/facts';
+import History from './components/pages/history';
+import Origin from './components/pages/origin';
+import Sights from './components/pages/sights';
+import Food from './components/pages/food';
 import FilteredPlayers from './components/pages/players';
+//includes
+import './Assets/css/default.min.css'
 
 
-ReactDOM.render(<FilteredPlayers/>
-	, document.getElementById('root')
+ReactDOM.render(
+      <Router>
+      <div className="App">
+      
+      <Header />
+
+      	<Route exact path='/' component={Homepage} />
+      	<Route exact path='/team' component={Team} />
+        <Route exact path='/facts' component={Facts} />
+        <Route exact path='/history' component={History} />
+        <Route exact path='/origin' component={Origin}/>
+        <Route exact path='/sights' component={Sights}/>
+        <Route exact path='/food' component={Food}/>
+        <Route exact path='/players' component={FilteredPlayers}/>
+      </div>
+      </Router>,
+ document.getElementById('root')
 
 );
 
